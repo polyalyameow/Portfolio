a6037a0d276cab87ae37cd255b5dcdfcfae57cf1
polyalyameow <117765092+polyalyameow@users.noreply.github.com>
2023-09-29 10:49:55 +0200

    cards with projects -- work in progress

--- src/components/Projects/ProjectsContent.tsx
+++ src/components/Projects/ProjectsContent.tsx
@@ -1,5 +1,6 @@
-import {useState, useRef} from 'react';
-import {projects} from "./ProjectsDb"
+
+import {projects} from "./ProjectsDb";
+import "./Projects.css";
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -13,6 +14,13 @@ import Button from '@mui/material/Button';
 import GitHub from '@mui/icons-material/GitHub';
 import NorthEastIcon from '@mui/icons-material/NorthEast';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 
 const ProjectsContent = () => {
@@ -25,10 +33,35 @@ const ProjectsContent = () => {
 
 
   return (
-    <Box sx={{display:"flex", flexDirection:"row", flexWrap: "wrap", justifyContent:"center"}}>
+
+    
+    <Box sx={{display:"flex", flexDirection:"row", flexWrap: "wrap", justifyContent:"center", width: "700px"}}>
+            <Swiper
+    effect={'coverflow'}
+    grabCursor={true}
+    centeredSlides={true}
+    loop={true}
+    slidesPerView={'auto'}
+    coverflowEffect={{
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    }}
+    pagination={{ el: '.swiper-pagination', clickable: true }}
+    navigation={{
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      clickable: true,
+    }}
+    modules={[EffectCoverflow, Pagination, Navigation]}
+    className="swiper_container"
+  >                
         {projects.map((project, id) => (
-            <Card key={id} sx={{ width: 300, height: 450, display:"flex", flexDirection: "column", margin: "5px", padding: "10px", borderRadius:"10px", border: "1px solid orange", justifyContent:"space-between"}}>
-                <CardMedia sx={{ height: 200, objectFit:"contain", borderRadius:"10px"}} image={project.image}></CardMedia>
+
+            <SwiperSlide >
+             <Card key={id} sx={{ width: 400, height: 550, display:"flex", flexDirection: "column", margin: "5px", padding: "30px", borderRadius:"10px", justifyContent:"space-between"}}>
+                <CardMedia sx={{ height: 300, objectFit:"contain", borderRadius:"10px"}} image={project.image}></CardMedia>
                 <CardContent sx={{display:"flex", flexDirection:"column",justifyContent:"space-between", alignItems:"center", border:"1px solid yellowgreen", maxHeight: "150px", height:"100%", padding:"0"}}>
                     <Typography>{project.name}</Typography>
                     <Typography sx={{textAlign:"center"}}>{project.description}</Typography>
@@ -50,12 +83,14 @@ const ProjectsContent = () => {
                     
                 </CardContent>
             </Card>
+            </SwiperSlide>
+            
         ))}
-        <Box>
-
-
-        </Box>
+      
+      </Swiper> 
     </Box>
+   
+    
   )
 }
 
